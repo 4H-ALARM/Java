efcd470bc0f5b1d770b2ec804e3879797225ccae
Unknown <fkalmbach@comcast.net>
2018-03-05 20:53:18 -0500

    Bicycle Break
    Hold right trigger to go half speed, both triggers to go quarter speed.  Not tested with actually driving yet

--- src/org/usfirst/frc2079/Java/subsystems/DriveTrain.java
+++ src/org/usfirst/frc2079/Java/subsystems/DriveTrain.java
@@ -25,13 +25,29 @@ public class DriveTrain extends Subsystem {
     }
 
     public void drive(Joystick controller/*L, Joystick controllerR*/){ //Control the left side with the left joystick and the the right side with the right joystick
-    	double mult = 1;
+    	double mult = 1.0;  // assume that no scaling is needed
+    	double bias = 2.5;  // sets the taper on the hyperbolic curve
+    	int p = 1; // 1st order
+    	double axis1 = controller.getRawAxis(1);  // read 1st joystick axis
+    	double axis2 = controller.getRawAxis(5);  // read 2nd joystick axis
+    	double d1 = Math.pow(axis1, p);  // default using a power curve for drive input
+    	double d2 = Math.pow(axis2, p);
+    	boolean isHyperbolic = false;  // select hyperbolic or power function
+    	
     	if (controller.getRawAxis(3) == 0 && controller.getRawAxis(2) == 0) {
+    		// scale down the input
     		mult = 0.8;
-    	} else {
-    		mult= 1;
+    	} 
+    	
+    	if (isHyperbolic) {
+    		// Use hyperbolic rather than power function
+    		d1 = Math.tanh(axis1*bias);
+    		d2 = Math.tanh(axis2*bias);       		
     	}
-    	motors.tankDrive(-(Math.pow(controller.getRawAxis(1), 3)*mult),-(Math.pow(controller.getRawAxis(5), 3)*mult)); //Puts values on cubic function to throttle speed. Also halves speed unless both triggers are held
+    	
+    	// Puts values for throttle speed. Also scales speed unless both triggers are held. 
+    	// Input is inverted to match the forward and reverse on the motors
+    	motors.tankDrive(-(d1*mult),-(d2*mult)); 
     }
     
     public void stop(){ //Stops motors
