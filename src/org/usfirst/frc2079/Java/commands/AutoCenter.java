e6676d6c6d3c8ec3d5e98e3bbd92c4b7fafff332
Unknown <fkalmbach@comcast.net>
2018-03-20 20:01:23 -0400

    Experimental Auto (cont.)

--- src/org/usfirst/frc2079/Java/commands/AutoCenter.java
+++ src/org/usfirst/frc2079/Java/commands/AutoCenter.java
@@ -1,62 +1,10 @@
 package org.usfirst.frc2079.Java.commands;
 
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.Preferences;
-import edu.wpi.first.wpilibj.Sendable;
-import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
-import edu.wpi.first.wpilibj.command.Command;
-import org.usfirst.frc2079.Java.Robot;
-import org.usfirst.frc2079.Java.RobotMap;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.command.CommandGroup;
 
-public class AutoCenter extends Command {
-
-	private long start;
-	private double tDrive, tEject;
-	private double endTime;
-
+public class AutoCenter extends CommandGroup{
 	public AutoCenter() {
-	}
-
-	@Override
-	protected void initialize() {
-		start = System.currentTimeMillis(); // Gets the current time in milliseconds
-		tDrive = 500;
-		tEject = 200;
-		endTime = tDrive+tEject;
-	}
-
-	@Override
-
-	protected void execute() {
-			if  (System.currentTimeMillis() < (start + tDrive)){
-				if (DriverStation.getInstance().getGameSpecificMessage().charAt(0) == 'L') {
-					RobotMap.dtSCG1.set(0.77); //Drift to left
-					RobotMap.dtSCG2.set(-0.80);
-				} else {
-					RobotMap.dtSCG1.set(0.79); //Drift to right
-					RobotMap.dtSCG2.set(-0.78);
-				}
-			}			
-			if (System.currentTimeMillis() > (start + tDrive) && (System.currentTimeMillis() < (start + endTime))) {
-				Robot.claw.spinWheels(-1.0); //Eject
-			}
-	}
-
-	@Override
-	protected boolean isFinished() {
-		return (System.currentTimeMillis() < (start + endTime));
-	}
-
-	@Override
-	protected void end() {
-		RobotMap.dtSCG1.set(0);
-		RobotMap.dtSCG2.set(0);
-		Robot.claw.spinWheels(0.0);
-	}
-
-	@Override
-	protected void interrupted() {
-		end();
+		addParallel(new DriveCenter());
+		addParallel(new PlayingPosition());
 	}
 }
--- src/org/usfirst/frc2079/Java/commands/AutoRight.java
+++ src/org/usfirst/frc2079/Java/commands/AutoRight.java
@@ -1,64 +1,10 @@
 package org.usfirst.frc2079.Java.commands;
 
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.Preferences;
-import edu.wpi.first.wpilibj.Sendable;
-import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
-import edu.wpi.first.wpilibj.command.Command;
-import org.usfirst.frc2079.Java.Robot;
-import org.usfirst.frc2079.Java.RobotMap;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.command.CommandGroup;
 
-public class AutoRight extends Command {
-
-	private long start;
-	private double tForwards, tTurn, tEject;
-	private double endTime;
-
+public class AutoRight extends CommandGroup{
 	public AutoRight() {
-	}
-
-	@Override
-	protected void initialize() {
-		start = System.currentTimeMillis(); // Gets the current time in milliseconds
-		tForwards = 1000;
-		tTurn = 400;
-		tEject = 200;
-		endTime = tForwards + tTurn + tEject;
-	}
-
-	@Override
-
-	protected void execute() {
-			if  (System.currentTimeMillis() < (start + tForwards)){
-				RobotMap.dtSCG1.set(0.79); //Forwards
-				RobotMap.dtSCG2.set(-0.80);
-			}
-				
-			if (System.currentTimeMillis() > (start + tForwards+200) && (System.currentTimeMillis() < (start + (tForwards+tTurn)))) {
-				RobotMap.dtSCG1.set(-0.79); //Turn to the left
-				RobotMap.dtSCG2.set(-0.80);
-			}
-				
-			if (System.currentTimeMillis() > (start + (tForwards+tTurn)) && System.currentTimeMillis() < (start + endTime)) {
-				Robot.claw.spinWheels(-1.0); //Eject
-			}
-	}
-
-	@Override
-	protected boolean isFinished() {
-		return (System.currentTimeMillis() < (start + endTime));
-	}
-
-	@Override
-	protected void end() {
-		RobotMap.dtSCG1.set(0);
-		RobotMap.dtSCG2.set(0);
-		Robot.claw.spinWheels(0.0);
-	}
-
-	@Override
-	protected void interrupted() {
-		end();
+		addParallel(new DriveRight());
+		addParallel(new PlayingPosition());
 	}
 }
